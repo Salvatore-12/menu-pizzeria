4e609ecbf8ed05306f2cbcbd7af88956c77a76fd
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-08 14:52:37 +0100

    Initial commit

---
+++ src/main/java/salvatoreasssennato/menupizzeria/MenuPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package salvatoreasssennato.menupizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/salvatoreasssennato/menupizzeria/MenuPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package salvatoreasssennato.menupizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
